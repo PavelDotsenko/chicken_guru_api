c4ee371d3138f56ddd79dcbc0f9703846572ba45
Pavel Dotsenko <pavel_dotsenko@hotmail.com>
2023-01-13 17:55:20 +0600

    accounts state

--- lib/chicken_guru_api/repository/account.ex
+++ lib/chicken_guru_api/repository/account.ex
@@ -1,8 +1,11 @@
 defmodule CG.Repository.Account do
   use Ecto.Schema
   import Ecto.Changeset
+  import EctoEnum
 
   alias CG.Repository.{Image, Language}
+
+  defenum State, deleted: 0, active: 1, blocked: 2
 
   schema "accounts" do
     field(:about, :string)
@@ -10,7 +13,8 @@ defmodule CG.Repository.Account do
     field(:is_admin, :boolean, default: false)
     field(:name, :string)
     field(:password, :string)
-    field(:state, :integer)
+    field(:repassword, :string, virtual: true)
+    field(:state, State, default: :active)
 
     belongs_to(:avatar, Image)
     belongs_to(:language, Language)
@@ -22,6 +26,6 @@ defmodule CG.Repository.Account do
   def changeset(account, attrs) do
     account
     |> cast(attrs, [:email, :password, :name, :about, :is_admin, :state, :avatar_id, :language_id])
-    |> validate_required([:email, :password, :name, :about, :is_admin, :state])
+    |> validate_required([:email, :password, :is_admin, :language_id])
   end
 end
